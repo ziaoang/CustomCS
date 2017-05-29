f6a219968e253f8efc2379c3ef552e43140ee087
ziaoang <ziaoang@gmail.com>
2017-05-29 20:13:10 +0800

    add login scene and register scene

---
+++ Assets/Custom Assets/Scripts/ButtonClick.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ButtonClick : MonoBehaviour {
+
+	InputField username;
+	InputField password;
+
+	void Start () {
+		foreach(Transform t in this.transform.GetComponentsInChildren<Transform>()) {
+			if (t.name.CompareTo ("Username") == 0) {
+				username = t.GetComponent<InputField> ();
+			}
+			else if (t.name.CompareTo ("Password") == 0) {
+				password = t.GetComponent<InputField> ();
+			}
+		}
+	}
+
+	public void LoginSceneLoginButtonClick() {
+		Debug.Log ("login login");
+		Debug.Log (username.text);
+		Debug.Log (password.text);
+		SceneManager.LoadScene ("Main");
+	}
+	public void LoginSceneRegisterButtonClick() {
+		Debug.Log ("login register");
+		SceneManager.LoadScene ("Register");
+	}
+	public void RegisterSceneRegisterButtonClick() {
+		Debug.Log ("register register");
+		Debug.Log (username.text);
+		Debug.Log (password.text);
+		SceneManager.LoadScene ("Main");
+	}
+	public void RegitserSceneLoginButtonClick() {
+		Debug.Log ("register login");
+		SceneManager.LoadScene ("Login");
+	}
+}
